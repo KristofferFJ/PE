95a07278254ba9302647b5335047bffda635383f
Kristoffer Fage Jensen <kristofferfage@gmail.com>
2018-01-25 12:16:19 +0100

    Update 587 - Concave triangle.py

--- 587 - Concave triangle.py
+++ 587 - Concave triangle.py
@@ -1,7 +1,7 @@
 #587 - Concave triangle
 #We find the intersection and integrate the function.
-#Circle is y = - sqrt(1-x^2) (if centered in (0,0))
-#And line is y = 1/n * x + 1/n - 1 (where n is number of circles.)
+#Circle is y = - sqrt(1-x^2) + 1 (if centered in (0,1))
+#And line is y = 1/n * x + 1/n (where n is number of circles.)
 #Intersection is then: x = (-sqrt(2) * n**(3/2) + n - 1)/(n**2 + 1)
 
 from math import *
