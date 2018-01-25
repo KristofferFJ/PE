45a3e3afc4c1f1cacc8f7d9cbb75242a1daef834
KristofferFJ <kristofferfage@gmail.com>
2018-01-25 12:10:15 +0100

    Solution to 587 by integration

---
+++ 587 - Concave triangle.py
@@ -0,0 +1,31 @@
+#587 - Concave triangle
+#We find the intersection and integrate the function.
+#Circle is y = - sqrt(1-x^2) (if centered in (0,0))
+#And line is y = 1/n * x + 1/n - 1 (where n is number of circles.)
+#Intersection is then: x = (-sqrt(2) * n**(3/2) + n - 1)/(n**2 + 1)
+
+from math import *
+from scipy import integrate
+
+def f(x):
+	return (- sqrt(1 - x**2) + 1)
+
+def intersection(n):
+	return (-sqrt(2) * n**(3/2) + n - 1)/(n**2 + 1)
+	
+#So we can return the result as a percentage of the area.
+area = (integrate.quad(f, -1, 0)[0])
+
+#The integral under the curve for some n
+def int(n):
+	res = integrate.quad(f, intersection(n), 0)[0]
+	#+ the triangle:
+	res += (intersection(n) - (-1))*(1/n * intersection(n) + 1/n)/2
+	return res/(area)
+
+i = 1
+while int(i) > 0.001:
+	i += 1
+
+print (i)
+	
